2e788b49f48f2cd59478c1f6080b8e143fd59a58
Archii0 <archiedascombe@icloud.com>
2024-12-06 23:53:31 +0000

    C template

---
+++ templates/default.c/PROJECTNAME.c
@@ -0,0 +1,8 @@
+/* PROJECTNAME.c*/
+
+#include "PROJECTNAME.h"
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
---
+++ templates/default.c/PROJECTNAME.h
@@ -0,0 +1,11 @@
+/* PROJECTNAME.h */
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+
+int main(int, char **);
