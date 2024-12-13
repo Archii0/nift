72c9eb24f07d4eedd43e87bfddbb2a05b2319e92
Archii0 <archiedascombe@icloud.com>
2024-12-13 22:00:30 +0000

    fixed issues with subdirectories

--- nift.sh
+++ nift.sh
@@ -7,14 +7,15 @@ usage() {
 
 pname=$1
 pdir="./${pname}"
-tdir=/var/nift/templates
+tdir=~/Repos/nift/templates
+# tdir=/var/nift/templates
 
 if [ -z "$pname" ]; then
     usage
     exit 1
 elif ! [ -d "$tdir" ]; then
     >&2 echo "Unable to find template directory: $tdir"
-    exit 2
+exit 2
 elif [ -d "$pname" ]; then
     >&2 echo "Project directory already exists: $pdir"
     exit 3
@@ -22,7 +23,7 @@ fi
 
 cur="$PWD"
 cd $tdir
-echo "Please select a template"
+echo "Please select a template:"
 
 select x in *; do
     template="$x"
@@ -33,19 +34,51 @@ cd $cur
 cp -R ${tdir}/$template $pdir
 cd $pdir
 
-for x in *; do
-    new=$(sed "s,PROJECTNAME,$pname,g" <<< "$x")
+echo "project dir :$pdir"
 
-    if [ "$x" = "$new" ]; then
-        sed "s,PROJECTNAME,$pname,g" < $x > temp
-        mv -f temp $x
+processfile() {
+    local file=$1
+    new=$(sed "s,PROJECTNAME,$pname,g" <<< "$file")
+
+    if [ "$file" = "$new" ]; then
+        sed "s,PROJECTNAME,$pname,g" < $file > temp
+        mv -f temp $file
     else
-        sed "s,PROJECTNAME,$pname,g" < $x > $new
+        sed "s,PROJECTNAME,$pname,g" < $file > $new
         if [ -e "$new" ]; then
-            rm -f $x
+            rm -f $file
         fi
     fi
+}
 
-done
+processdirectory() {
+    local dir=$1
+    echo "Processing directory: $dir"
+    curDir="$PWD"
+
+    if [ ! -d "$dir" ]; then
+        echo "not a dir"
+        exit 1
+    fi
+
+    for file in "$dir"/*; do
+        if [ -f "./$file" ]; then
+            processfile $file
+        elif [ -d "./$file" ]; then
+            base_dir=$(basename "$file")
+
+            if [[ "$base_dir" == *"PROJECTNAME"* ]]; then
+                new_dir_name="${base_dir//PROJECTNAME/$pname}"
+                mv "$file" "$new_dir_name"
+                file=$new_dir_name
+            fi
+            processdirectory $file
+        else
+            echo "$file is neither a file or a directory - please add as an issue on GitHub if this type should be supported."
+        fi
+    done
+}
+
+processdirectory "."
 
 echo "Created new project using $template. Pretty nifty."
